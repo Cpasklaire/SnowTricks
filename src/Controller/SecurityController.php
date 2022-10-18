955c63bf5a4a548d9f0797272f0f05acf3a516b7
Cpasklaire <80284454+Cpasklaire@users.noreply.github.com>
2022-10-18 13:33:26 +0200

    avant de faire du front

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,10 +9,15 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 use App\Form\FormUserType;
+use App\Form\FormValidType;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-//use App\Service\Mailer;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use App\Repository\UserRepository;
+use Symfony\Component\Security\Http\LoginLink\LoginLinkHandlerInterface;
+use Symfony\Component\Security\Http\LoginLink\LoginLinkNotification;
+use Symfony\Component\Notifier\Recipient\Recipient;
+use Symfony\Component\Notifier\NotifierInterface;
 
 class SecurityController extends AbstractController
 {
@@ -62,32 +67,69 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/confirmer-mon-compte/{token}', name: 'confirm_account')]
-    public function confirmAccount(string $token, EntityManagerInterface $manager)
-    {
-        $user = $this->userRepository->findOneBy(["token" => $token]);
+    public function confirmAccount(string $token, Request $request, EntityManagerInterface $manager, UserRepository $userRepo)
+    { 
+        $user = $userRepo->findOneBy(['token' => $token]);
 
-        if($user) {
-            $user->setToken(null);
-            $user->setActivate(true);
+        $formValid = $this->createForm(FormValidType::class, $user);
+        $formValid->handleRequest($request);
+
+        if($formValid->isSubmitted() && $formValid->isValid()){
+            $user->setToken(null)
+                ->setActivate(true);
 
             $manager->persist($user);
             $manager->flush();
 
             $this->addFlash("flash", "Compte actif ! Connectez vous ! ");
             return $this->redirectToRoute("login");
-        } else {
-            $this->addFlash("flash", "Ce compte n'exsite pas");
-            return $this->redirectToRoute('home');
-        }
+        } 
+        return $this->render('security/confirmToken.html.twig', [
+            'formValid' => $formValid->createView()
+        ]);
     }
 
-
-    #[Route('/login', name: 'login')]
-    public function login() {
-
-        return $this->render('security/login.html.twig');
+    #[Route('/login_check', name: 'login_check')]
+    public function login_check() {
     }
 
     #[Route('/logout', name: 'logout')]
     public function logout() {}
+
+    #[Route('/forgot-pass', name: 'forgot-pass')] // email configuration
+    public function requestLoginLink(NotifierInterface $notifier, LoginLinkHandlerInterface $loginLinkHandler, UserRepository $userRepository, Request $request)
+    {
+        // check if login form is submitted
+        if ($request->isMethod('POST')) {
+            // load the user in some way (e.g. using the form input)
+            $email = $request->request->get('email');
+            $user = $userRepository->findOneBy(['email' => $email]);
+
+            // create a login link for $user this returns an instance
+            // of LoginLinkDetails
+            $loginLinkDetails = $loginLinkHandler->createLoginLink($user);
+            $loginLink = $loginLinkDetails->getUrl();
+
+            $notification = new LoginLinkNotification(
+                $loginLinkDetails,
+                'Welcome to MY WEBSITE!' // email subject
+            );
+            // create a recipient for this user
+            $recipient = new Recipient($user->getEmail());
+
+            // send the notification to the user
+            $notifier->send($notification, $recipient);
+
+            // render a "Login link is sent!" page
+            return $this->render('security/login_link_sent.html.twig');
+
+        }
+        return $this->render('security/forgotPassword.html.twig');
+    }
+
+    #[Route('/login', name: 'login')]
+    public function login() {
+        /* SAM seule user.activate = true peuvent ce connecter */
+        return $this->render('security/login.html.twig');
+    }
 }
